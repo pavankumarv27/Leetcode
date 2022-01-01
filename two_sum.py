6a2b15d64398b2e38aadba19433b2c54d4d1a8b3
pavankumarv27 <pavankumarv026@gmail.com>
2022-01-02 00:45:15 +0530

    updated hard coded value (9) by target variable.

--- two_sum.py
+++ two_sum.py
@@ -36,7 +36,7 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == 9:
+                if nums[i] + nums[j] == target:
                     return [i, j]
         return [0, 0]
 
